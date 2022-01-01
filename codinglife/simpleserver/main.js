e96cbf7621c329b1aba98de7256d4f04c4d0d58b
truespring <truespring1@gmail.com>
2022-01-01 16:37:28 +0900

    [feat] add to HTML form tag

--- codinglife/simpleserver/main.js
+++ codinglife/simpleserver/main.js
@@ -13,6 +13,7 @@ function templateHTML(title, list, body) {
 	<body>
 		<h1><a href="/">WEB</a></h1>
 		${list}
+		<a href="/create">create</a>
 		${body}
 	</body>
 	</html>
@@ -61,9 +62,30 @@ var app = http.createServer(function(req, res) {
 				});
 			});
 		}
+	} else if (pathname === '/create') {
+		fs.readdir('./data', function (err, filelist) {
+			title = 'WEB - create';
+			list = templateList(filelist);
+			template = templateHTML(title, list, 
+				`
+				<form action="http://localhost:3000/process_create" method="post">
+					<p>
+						<input type="text" name="title" placeholder="title">
+					</p>
+					<p>
+						<textarea name="description" placeholder="description"></textarea>
+					</p>
+					<p>
+						<input type="submit">
+					</p>
+				</form>
+				`);
+			res.writeHead(200);
+			res.end(template);
+		});
 	} else {
-		response.writeHead(404);
-		response.end('Not found');
+		res.writeHead(404);
+		res.end('Not found');
 	}
 });
 app.listen(3000);

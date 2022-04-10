f96506f944fa873af934b4a6df3e145747e4ab63
truespring <truespring1@gmail.com>
2022-04-10 23:03:53 +0900

    [express] add to permission

---
+++ board/routes/posts.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post');
+const util = require('../util');
+
+// Index
+router.get('/', (req, res) => {
+    Post.find({})
+        .populate('author')
+        .sort('-craeteAt')
+        .exec((err, posts) => {
+            if (err) return res.json(err);
+            res.render('posts/index', {posts: posts});
+        });
+});
+
+// New
+router.get('/new', util.isLoggedin, (req, res) => {
+    const post = req.flash('post')[0] || {};
+    const errors = req.flash('errors')[0] || {};
+    res.render('posts/new', {post: post, errors: errors});
+});
+
+// create
+router.post('/', util.isLoggedin, (req, res) => {
+    req.body.author = req.user._id;
+    Post.create(req.body, (err, post) => {
+        if (err) {
+            req.flash('post', req.body);
+            req.flash('errors', util.parseError(err));
+            return res.redirect('/posts/new');
+        }
+        res.redirect('/posts');
+    });
+});
+
+// show
+router.get('/:id', (req, res) => {
+    Post.findOne({_id: req.params.id})
+        .populate('author')
+        .exec((err, post) => {
+            if (err) return res.json(err);
+            res.render('posts/show', {post: post});
+        });
+});
+
+// edit
+router.get('/:id/edit', util.isLoggedin, checkPermission, (req, res) => {
+    const post = req.flash('post')[0];
+    const errors = req.flash('errors')[0] || {};
+    if (!post) {
+        Post.findOne({_id: req.params.id}, (err, post) => {
+            if (err) return res.json(err);
+            res.render('posts/edit', {post: post, errors: errors});
+        });
+    } else {
+        post._id = req.params.id;
+        res.render('posts/edit', {post: post, errors: errors});
+    }
+});
+
+// update
+router.put('/:id', util.isLoggedin, checkPermission, (req, res) => {
+    req.body.updateAt = Date.now();
+    Post.findOneAndUpdate({_id: req.params.id}, req.body, {runValidators: true}, (err, post) => {
+        if (err) {
+            req.flash('post', req.body);
+            req.flash('errors', util.parseError(err));
+            return res.redirect(`/posts/${req.params.id}/edit`);
+        }
+        res.redirect(`/posts/${req.params.id}`);
+    });
+});
+
+// destroy
+router.delete('/:id', util.isLoggedin, checkPermission, (req, res) => {
+    Post.deleteOne({_id: req.params.id}, (err) => {
+        if (err) return res.json(err);
+        res.redirect('/posts');
+    });
+});
+
+module.exports = router;
+
+// private functions
+function checkPermission(req, res, next) {
+    Post.findOne({_id:req.params.id}, (err, post) => {
+        if (err) return res.json(err);
+        if (post.author !== req.user._id) return util.noPermission(req, res);
+
+        next();
+    });
+}
---
+++ board/routes/users.js
@@ -0,0 +1,111 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+const util = require('../util');
+
+// index
+router.get('/', (req, res) => {
+    User.find({})
+        .sort({username: 1})
+        .exec((err, users) => {
+            if (err) return res.json(err);
+            res.render('users/index', {users: users});
+        });
+});
+
+// New
+router.get('/new', (req, res) => {
+    const user = req.flash('user')[0] || {};
+    const errors = req.flash('errors')[0] || {};
+    res.render('users/new', {user: user, errors: errors});
+});
+
+// create
+router.post('/', (req, res) => {
+    User.create(req.body, (err, user) => {
+        if (err) {
+            req.flash('user', req.body);
+            req.flash('errors', parseError(err));
+            return res.redirect('/users/new');
+        }
+        res.redirect('/users');
+    });
+});
+
+// show
+router.get('/:username', util.isLoggedin, checkPermission, (req, res) => {
+    User.findOne({username: req.params.username}, (err, user) => {
+        if (err) return res.json(err);
+        res.render('users/show', {user: user});
+    });
+});
+
+// edit
+router.get('/:username/edit', util.isLoggedin, checkPermission, (req, res) => {
+    const user = req.flash('user')[0];
+    const errors = req.flash('errors')[0] || {};
+    if (!user) {
+        User.findOne({username: req.params.username}, (err, user) => {
+            if (err) return res.json(err);
+            res.render('users/edit', {username: req.params.username, user: user, errors: errors});
+        });
+    } else {
+        res.render('users/edit', {username: req.params.username, user: user, errors: errors});
+    }
+});
+
+// update
+router.put('/:username', util.isLoggedin, checkPermission, (req, res, next) => {
+    User.findOne({username: req.params.username})
+        .select('password')
+        .exec((err, user) => {
+            if (err) return res.json(err);
+
+            // update user object
+            user.originalPassword = user.password;
+            user.password = req.body.newPassword ? req.body.newPassword : user.password;
+            for (let p in req.body) {
+                user[p] = req.body[p];
+            }
+
+            // save updated user
+            user.save((err, user) => {
+                if (err) {
+                    req.flash('user', req.body);
+                    req.flash('errors', util.parseError(err));
+                    return res.redirect(`/users/${req.params.username}/edit`);
+                }
+                res.redirect(`/users/${user.username}`);
+            });
+        });
+});
+
+module.exports = router;
+
+// functions
+function parseError(errors) {
+    const parsed = {};
+
+    if (errors.name === 'ValidationError') {
+        for (let name in errors.errors) {
+            const validationError = errors.errors[name];
+            parsed[name] = {message: validationError.message};
+        }
+    } else if (errors.code === '11000' && errors.ermsg.indexOf('username') > 0) {
+        parsed.username = {message: 'This username already exists!'};
+    } else {
+        parsed.unhandled = JSON.stringify(errors);
+    }
+
+    return parsed;
+}
+
+// private functions
+function checkPermission(req, res, next) {
+    User.findOne({username:req.params.username}, (err, user) => {
+        if (err) return res.json(err);
+        if (user.id !== req.user.id) return util.noPermission(req, res);
+
+        next();
+    });
+}
